0cf91dc5a10a8baa8c492477cc4761abf870f587
scxpn <ubirajara2018@gmail.com>
2021-01-09 00:21:12 -0300

    Nav menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -9,13 +9,14 @@ function Header() {
   return (
     <div id="header">
       <div className="primary-area">
-        <MenuBars />
-        
-        <div className='profile-photo'>
-          <Link to="/" className="landing-main-text">
-            <img src={'https://avatars0.githubusercontent.com/u/50757312?s=460&u=f6cfeb6e4d6d727059382f52226d81824640c555&v=4'} alt="Ubirajara Junior"/>
-          </Link>
-                    
+        <div className="main-header">
+          <MenuBars />
+          <div className='profile-photo'>
+            <Link to="/" className="landing-main-text">
+              <img src={'https://avatars0.githubusercontent.com/u/50757312?s=460&u=f6cfeb6e4d6d727059382f52226d81824640c555&v=4'} alt="Ubirajara Junior"/>
+            </Link>
+                      
+          </div>
         </div>
 
         <div className="landing-text">
--- src/components/MenuBars/index.jsx
+++ src/components/MenuBars/index.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
 import { GoThreeBars } from 'react-icons/go';
+import { AiOutlineArrowLeft } from 'react-icons/ai';
 
 import './index.css';
 
-function MenuBars() {
-  return (
-    <div id="menu-bars">
-      <GoThreeBars size={30} className="three-bars" />
+
+class MenuBars extends React.Component {
+
+  state = { showMenu: false }
+
+  toggleMenu = () => {
+  this.setState({
+    showMenu: !this.state.showMenu
+  })
+  }
+
+  render() {
+  const barsButton = this.state.showMenu ? 'transparent' : 'white';
+  const arrowButton = this.state.showMenu ? 'white' : 'transparent';
+  const menu = this.state.showMenu ? 'active' : '';
+    return (     
+  <nav id="navbar">
+    
+    <div className="navbar-brand">
+      <GoThreeBars className="bars-button" size={25} color={barsButton} onClick={this.toggleMenu} />
+
+      <div className={`navbar-burger-${menu}`}>
+        <div className="itens-menu">
+          
+            <AiOutlineArrowLeft className="arrow-button" size={25} color={arrowButton} onClick={this.toggleMenu} />
+         
+        </div>
+      </div>
     </div>
-  );
+  </nav>)
+  }
 };
 
 export default MenuBars;
